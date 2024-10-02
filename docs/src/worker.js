a62bf220d79ea7727fdb9fc0f18e4aa4ee832dae
Me <95430990+m1kecraft@users.noreply.github.com>
2024-10-02 06:45:32 -0700

    Fix worker.js

--- docs/src/worker.js
+++ docs/src/worker.js
@@ -1,8 +1,8 @@
 importScripts("https://cdn.jsdelivr.net/npm/xterm-pty@0.9.4/workerTools.js");
-importScripts(location.origin + "/container2wasm-demo" + "/src/browser_wasi_shim/index.js");
-importScripts(location.origin + "/container2wasm-demo" + "/src/browser_wasi_shim/wasi_defs.js");
-importScripts(location.origin + "/container2wasm-demo" + "/src/worker-util.js");
-importScripts(location.origin + "/container2wasm-demo" + "/src/wasi-util.js");
+importScripts("./src/browser_wasi_shim/index.js");
+importScripts("./src/browser_wasi_shim/wasi_defs.js");
+importScripts("./src/worker-util.js");
+importScripts("./src/wasi-util.js");
 
 onmessage = (msg) => {
     if (serveIfInitMsg(msg)) {
